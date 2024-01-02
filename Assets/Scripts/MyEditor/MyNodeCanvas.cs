8e7361485dfdc17f149fe03e5af3651cac3c45f5
yangpei1010110 <970387216@qq.com>
2024-01-02 14:22:41 +0800

    Add Odin

---
+++ Assets/Scripts/MyEditor/MyNodeCanvas.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace MyEditor
+{
+    public class MyNodeCanvas : MonoBehaviour
+    {
+        private bool _isHide;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
